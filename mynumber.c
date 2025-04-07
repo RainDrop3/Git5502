b506372912c76691db2875a9f61c353c7872f730
RainDrop3 <kanghy6220@gmail.com>
2025-04-07 22:20:05 +0900

    modified mynumber.c

--- mynumber.c
+++ mynumber.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	for(int i=1; i<51; i++) printf("%d\n", 2i-1);
+	for(int i=1; i<51; i++) printf("%d\n", 2*i-1);
+	for(int j=1; j<51; j++) printf("%d\n", 2*j);
 	return 0;
 }
